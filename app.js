074501c6d4645406c08979ff200f08769bf1206f
Fabricio <fabriciocandelino@gmail.com>
2021-08-13 15:00:56 -0300

    2nd commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Louder.'
+    })
+});
+
+app.get('/artists', (req,res) => {
+    res.render('pages/artists', {
+      title: 'Louder. | Artists'
+    });
+  });
+
+app.listen(3000, () => {
+    console.log('Running Port 3000');
+});
